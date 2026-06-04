96d256f7529210a920493c5aa48ff89bb6f45e65
jatinswarnkar <jatin.swarnkar@perceptiviti.com>
2026-06-04 10:31:31 +0530

    Remove apt-get from startup script to allow app to boot

--- startup.sh
+++ startup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Prevent interactive prompts (like timezone selection) from hanging the installation
-export DEBIAN_FRONTEND=noninteractive
-
-# Install ffmpeg (not included in Azure App Service Python runtime)
-apt-get update && apt-get install -y --no-install-recommends ffmpeg
-
 # Run migrations
 python manage.py migrate --noinput
 
